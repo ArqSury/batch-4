8ba11bfac722abc83db0e9b7c9e6f176e14bf850
Ariq Surya Wardhana <Ariqsurya0510@gmail.com>
2025-10-08 08:05:15 +0700

    Add files via upload

--- tugas_2.dart
+++ tugas_2.dart
@@ -1,23 +1,29 @@
-import'dart:io';
+import 'dart:io';
 
-void main(){
+void main() {
   stdout.write("Masukan Nilai UTS Anda: ");
-  String ? inputUTS = stdin.readLineSync();
+  String? inputUTS = stdin.readLineSync();
   int uts = int.parse(inputUTS ?? "0");
   stdout.write("Masukan Nilai UAS Anda: ");
-  String ? inputUAS = stdin.readLineSync();
+  String? inputUAS = stdin.readLineSync();
   int uas = int.parse(inputUAS ?? "0");
   stdout.write("Masukan Kehadiran Anda: ");
-  String ? kehadiran = stdin.readLineSync();
+  String? kehadiran = stdin.readLineSync();
   double hadir = double.parse(kehadiran ?? "0") / 100;
-  
-  double rata_rata = (uts + uas) / 2;
-  print("Nilai Rata-Rata: $rata_rata");
 
-  if (rata_rata >= 70 && hadir >= 75 && uts >= 60 || uas >= 60){
-    print("Selamat Anda Lulus");  
-  } else{
-    print("Mohon Maaf Anda Lulus");
+  double rata_rata(uts, uas) {
+    final double nilaiAkhir = (uts = uas) / 2;
+    return nilaiAkhir;
   }
 
+  void kelulusan(uts, uas, kehadiran) {
+    final nilaiAkhir = rata_rata(uts, uas);
+    if (nilaiAkhir >= 70 && hadir >= 75 && uts >= 60 && uas >= 60) {
+      print("Selamat Anda Lulus");
+    } else {
+      print("Mohon Maaf Anda Tidak Lulus");
+    }
+  }
+
+  kelulusan(uts, uas, kehadiran);
 }
